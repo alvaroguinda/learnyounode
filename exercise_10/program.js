7bad7bf1c0f7af79aba9486cbf9ad0c7249ca1a1
Alvaro Guinda Rivero <alvaro.guinda.rivero@hp.com>
2017-06-23 13:39:14 +0200

    Exercise 10 "Time server" finished

---
+++ exercise_10/program.js
@@ -0,0 +1,23 @@
+var net = require('net')
+
+function zeroFill (i) {
+  return (i < 10 ? '0' : '') + i
+}
+
+function now () {
+  var date = new Date()
+  year = date.getFullYear()
+  month = zeroFill(date.getMonth() + 1)
+  day = zeroFill(date.getDate())
+  hour = zeroFill(date.getHours())
+  minutes = zeroFill(date.getMinutes())
+  result = year + "-" + month + "-" + day + " "
+  result += hour + "-" + minutes
+  return result
+}
+
+var server = net.createServer(function (socket) {
+  //socket handling logic
+  socket.end(now() + '\n')
+})
+server.listen(Number(process.argv[2]))
